3a4040646b5a3f34d5dc3a100d0d22ec239d9bcf
Lori K <soupfairy99@gmail.com>
2020-01-21 12:55:03 -0500

    add pwd

--- bash.js
+++ bash.js
@@ -3,6 +3,12 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data)=> {
     const cmd = data.toString().trim();
 
-    process.stdout.write('You typed: ' + cmd);
+    // process.stdout.write('You typed: ' + cmd);
+    // process.stdout.write('\nprompt > ');
+    if (cmd === 'pwd'){
+        process.stdout.write(process.cwd())
+    }
+
     process.stdout.write('\nprompt > ');
+
 })
